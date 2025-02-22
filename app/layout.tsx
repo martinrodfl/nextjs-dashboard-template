196f24fbaf3e0381ed7458cd1a7fb55e985dedd4
Martin Rodriguez <martinrodfl@gmail.com>
2025-02-22 13:25:35 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+// Esto se llama diseño de raíz y se requiere en cada siguiente aplicación.
+// Cualquier interfaz de usuario que agregue al diseño raíz se compartirá en todas las páginas de su aplicación.
+// Puede usar el diseño de la raíz para modificar su <html> y <body> Etiquetas y agregue metadatos
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
